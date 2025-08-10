a7570bd60aeb528c9801e61228d05a857d94ed6c
guiseppe <guiseppegnixon@gmail.com>
2025-08-09 21:22:52 -0400

    edits

--- profile/config/fonts.nix
+++ profile/config/fonts.nix
@@ -4,7 +4,6 @@
         fonts.packages = with pkgs; [
                 nerd-fonts.fira-code
                 noto-fonts-emoji
-                liberation-mono
                 fira-code
                 fira-code-symbols
                 noto-fonts
