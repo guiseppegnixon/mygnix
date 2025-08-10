fcc739983478b17076d4d6dbfaaf57d0bf96b70e
guiseppe <guiseppegnixon@gmail.com>
2025-08-10 09:57:32 -0400

    alacritty config II

--- profile/config/alacritty.nix
+++ profile/config/alacritty.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, inputs, flakeSettings, ... }:
+{ config, pkgs, inputs, flakeSettings, zsh, ... }:
 
 {
 
         programs.alacritty = {
                 enable = true;
+                shell.program = ${zsh}/bin/zsh;
                 settings = {
                         window = {
                                 opacity = 0.5;
                                 blur = true;
                                 title = flakeSettings.hostname;
                         };    
-                theme = "nord";
                 };
         };
 
