b67fcfeda634080912af4e159dca04c41cf04444
guiseppe <guiseppegnixon@gmail.com>
2025-08-12 13:46:27 -0400

    hardening refactor

--- profile/system/hardening/cronjobs.nix
+++ profile/system/hardening/cronjobs.nix
@@ -7,6 +7,8 @@
       "10 2 * * 0 root ${pkgs.chkrootkit}/bin/chkrootkit | logger -t chkrootkit"
       # Every day at 2:00 AM, run clamscan as root and append output to a log file
       "0 2 * * * root ${pkgs.clamav}/bin/clamscan -r /home >> /var/log/clamscan.log"
+      # Every day at 2:15 AM, run aide as root and append output to a log file
+                        #"0 2 * * * root ${pkgs.aide}/bin/aide"
     ];
   };
 }
