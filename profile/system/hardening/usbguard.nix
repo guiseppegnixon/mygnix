5473215f81fe756454093423451d0b5d9e9ed7bd
guiseppe <guiseppegnixon@gmail.com>
2025-08-13 19:48:10 -0400

    usbguard

--- profile/system/hardening/usbguard.nix
+++ profile/system/hardening/usbguard.nix
@@ -20,9 +20,9 @@ in {
       presentDevicePolicy = "allow";
       rules = ''
         # allow `only` devices with mass storage interfaces (USB Mass Storage)
-        allow with-interface equals { 08:*:* }
+        # allow with-interface equals { 08:*:* }
         # allow mice and keyboards
-        # allow with-interface equals { 03:*:* }
+        allow with-interface equals { 03:*:* }
 
         # Reject devices with suspicious combination of interfaces
         reject with-interface all-of { 08:*:* 03:00:* }
