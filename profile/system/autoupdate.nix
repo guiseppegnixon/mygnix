32fb1241ca626f0fb3e3d52d28ce080d05e29fe0
guiseppe <guiseppegnixon@gmail.com>
2025-08-13 14:35:54 -0400

    autoupdate options

--- profile/system/autoupdate.nix
+++ profile/system/autoupdate.nix
@@ -4,7 +4,7 @@
   #automatically deploy updates
   system.autoUpgrade = {
     enable = true;
-    randomizedDelaySec = "600"; #adds 0-10 minutes to trigger time to stagger updates
+    #randomizedDelaySec = "600"; #adds 0-10 minutes to trigger time to stagger updates
     operation = "boot"; #deploys update as new boot entry. use the default setting of "switch" for immediate effect.
     dates = "daily";
     flake = ( "/home" + ("/" + flakeSettings.username) + "/.nixfiles/flake.nix");
