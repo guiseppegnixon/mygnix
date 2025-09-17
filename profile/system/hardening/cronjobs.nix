1d78a5ee11d02ece3fa73df3ba8f840251a19270
guiseppe <guiseppegnixon@gmail.com>
2025-09-17 16:31:42 -0400

    pkg update

--- profile/system/hardening/cronjobs.nix
+++ profile/system/hardening/cronjobs.nix
@@ -3,8 +3,6 @@
     enable = true;
     # messages.enable = true;
     systemCronJobs = [
-      # Every Sunday at 2:10 AM, run chkrootkit as root, log output for review
-      "10 2 * * 0 root ${pkgs.chkrootkit}/bin/chkrootkit | logger -t chkrootkit"
       # Every day at 2:00 AM, run clamscan as root and append output to a log file
       "0 2 * * * root ${pkgs.clamav}/bin/clamscan -r /home >> /var/log/clamscan.log"
       # Every day at 2:15 AM, run aide as root and append output to a log file
