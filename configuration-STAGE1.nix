1ed4791803236d2b5cbfc5aba3a0e1884a7b8770
guiseppe <guiseppegnixon@gmail.com>
2025-09-23 14:15:16 -0400

    impermanence prelim

---
+++ configuration-STAGE1.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }: {
+	imports = [
+	  ./hardware-configuration.nix
+    ./persist.nix # This file now manages ALL persistent state
+  ];
+
+  # --- Security Hardening ---
+  networking.firewall.enable = true;
+
+  # --- Users ---
+  users.users.your-username = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "your-temporary-password";
+  };
+  
+  # --- System Settings ---
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+    
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  powerManagement.cpuFreqGovernor = "powersave";
+      
+  # --- Basic System Settings ---
+  time.timeZone = "America/New_York";
+  networking.hostName = "CHANGEMEBITCHPLEASE";
+  networking.networkmanager.enable = true;
+  environment.systemPackages = with pkgs; [ 
+	  neovim 
+	  git 
+	  sbctl
+	  wget
+	  curl
+	  tree
+ ];
+
+  system.stateVersion = "25.05"; 
+}
