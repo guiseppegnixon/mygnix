f57ae9be1235197f3818538621e528f63f975cab
guiseppe <guiseppegnixon@gmail.com>
2025-09-30 13:48:16 -0400

    touchup

--- profile/system/hardening/audit.nix
+++ profile/system/hardening/audit.nix
@@ -1,9 +1,7 @@
 { lib, ... }:
+
 let
-  rules = builtins.readFile (builtins.fetchurl {
-    url = "https://raw.githubusercontent.com/Neo23x0/auditd/d4a373a81ce3ea1930aa007ee32aa2f68c0b4db0/audit.rules";
-    sha256 = "sha256:0lj63zns55wk947xb47yak1sqzs21lm2bxzy8ncylzc546n20mh8";
-  });
+  rulesFileContent = builtins.readFile ./audit.rules;
 in
 {
   security.auditd.enable = lib.mkDefault true;
@@ -11,7 +9,7 @@ in
     enable = lib.mkDefault true;
     rules =
       let
-        lines = lib.strings.splitString "\n" rules;
+        lines = lib.strings.splitString "\n" rulesFileContent;
       in
       builtins.filter (line: (!lib.strings.hasPrefix "#" line) && line != "") lines;
   };
